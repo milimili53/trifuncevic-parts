bd849f1ad238f98f316b6cd6a44948aba3f38f2c
milimili53 <99088738+milimili53@users.noreply.github.com>
2023-03-22 14:28:35 +0100

    Add files via upload

---
+++ trifuncevic ad details/ad-script.js
@@ -0,0 +1,27 @@
+/*
+const infoParts = document.querySelectorAll(".icon-info");
+const adWrapper = document.querySelector(".ad-info-wrapper");
+const infoSec = document.querySelector(".info-part");
+const displayRows = document.querySelector(".display-rows");
+
+console.log(infoParts);
+
+if (infoParts.length > 8) {
+  const half = infoParts.length / 2;
+  // const firstHalf = infoParts.slice(0, 3);
+  // console.log(firstHalf);
+
+  // Create an empty array to hold the selected elements
+  const selectedElements = [];
+
+  // Loop through the original NodeList and select every other element
+  for (let i = 0; i < infoParts.length; i += half) {
+    selectedElements.push(infoParts.item(i));
+  }
+
+  // Create a new NodeList from the selected elements array
+
+  // const selectedList = document.createNodeList(...selectedElements);
+  console.log(selectedElements);
+}
+*/
